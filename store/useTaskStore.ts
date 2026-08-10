030467b249c8590c493c10159a70397f654fc980
filbertik <aton2019zvit@gmail.com>
2026-08-10 09:44:03 +0300

    component 1 content

---
+++ store/useTaskStore.ts
@@ -0,0 +1,41 @@
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+import { Task, TaskStatus } from "@/types/task.types";
+
+type TaskStore = {
+  tasks: Task[];
+  addTask: (title: string) => void;
+  updateTaskStatus: (id: string, status: TaskStatus) => void;
+};
+
+export const useTaskStore = create<TaskStore>()(
+  persist(
+    (set) => ({
+      tasks: [],
+
+      addTask: (title) =>
+        set((state) => ({
+          tasks: [
+            ...state.tasks,
+            {
+              id: crypto.randomUUID(),
+              title,
+              status: "todo",
+              priority: "medium",
+              createdAt: new Date().toISOString(),
+            },
+          ],
+        })),
+
+      updateTaskStatus: (id, status) =>
+        set((state) => ({
+          tasks: state.tasks.map((task) =>
+            task.id === id ? { ...task, status } : task,
+          ),
+        })),
+    }),
+    {
+      name: "task-storage",
+    },
+  ),
+);
