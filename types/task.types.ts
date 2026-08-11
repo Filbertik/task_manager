3a3d606e3791b598003af7a77cc00d71336d169b
filbertik <aton2019zvit@gmail.com>
2026-08-11 08:45:03 +0300

    try animate 1

--- types/task.types.ts
+++ types/task.types.ts
@@ -1,11 +1,25 @@
 export type TaskStatus = "todo" | "in-progress" | "done";
 
+export type TaskPriority = "low" | "medium" | "high";
+
 export type Task = {
   id: string;
   title: string;
   description?: string;
   status: TaskStatus;
-  priority: "low" | "medium" | "high";
+  priority: TaskPriority;
   createdAt: string;
-  moveTask: (id: string, status: TaskStatus) => void;
+  order: number;
 };
+
+// export type TaskStatus = "todo" | "in-progress" | "done";
+
+// export type Task = {
+//   id: string;
+//   title: string;
+//   description?: string;
+//   status: TaskStatus;
+//   priority: "low" | "medium" | "high";
+//   createdAt: string;
+//   moveTask: (id: string, status: TaskStatus) => void;
+// };
