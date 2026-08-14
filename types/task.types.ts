66a4b3eee2f07c8207e8b65c21ae1984867cf1c7
filbertik <aton2019zvit@gmail.com>
2026-08-14 10:49:46 +0300

    add duedate type

--- types/task.types.ts
+++ types/task.types.ts
@@ -9,17 +9,32 @@ export type Task = {
   status: TaskStatus;
   priority: TaskPriority;
   createdAt: string;
+  dueDate?: string;
   order: number;
 };
 
 // export type TaskStatus = "todo" | "in-progress" | "done";
+
+// export type TaskPriority = "low" | "medium" | "high";
 
 // export type Task = {
 //   id: string;
 //   title: string;
 //   description?: string;
 //   status: TaskStatus;
-//   priority: "low" | "medium" | "high";
+//   priority: TaskPriority;
 //   createdAt: string;
-//   moveTask: (id: string, status: TaskStatus) => void;
+//   order: number;
 // };
+
+// // export type TaskStatus = "todo" | "in-progress" | "done";
+
+// // export type Task = {
+// //   id: string;
+// //   title: string;
+// //   description?: string;
+// //   status: TaskStatus;
+// //   priority: "low" | "medium" | "high";
+// //   createdAt: string;
+// //   moveTask: (id: string, status: TaskStatus) => void;
+// // };
