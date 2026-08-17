28c8270a755d0706df8b468b42ac8a59b02e4ed1
filbertik <aton2019zvit@gmail.com>
2026-08-17 16:35:00 +0300

    edit types

--- types/task.types.ts
+++ types/task.types.ts
@@ -4,37 +4,18 @@ export type TaskPriority = "low" | "medium" | "high";
 
 export type Task = {
   id: string;
+
   title: string;
+
   description?: string;
+
   status: TaskStatus;
+
   priority: TaskPriority;
+
   createdAt: string;
+
   dueDate?: string;
+
   order: number;
 };
-
-// export type TaskStatus = "todo" | "in-progress" | "done";
-
-// export type TaskPriority = "low" | "medium" | "high";
-
-// export type Task = {
-//   id: string;
-//   title: string;
-//   description?: string;
-//   status: TaskStatus;
-//   priority: TaskPriority;
-//   createdAt: string;
-//   order: number;
-// };
-
-// // export type TaskStatus = "todo" | "in-progress" | "done";
-
-// // export type Task = {
-// //   id: string;
-// //   title: string;
-// //   description?: string;
-// //   status: TaskStatus;
-// //   priority: "low" | "medium" | "high";
-// //   createdAt: string;
-// //   moveTask: (id: string, status: TaskStatus) => void;
-// // };
